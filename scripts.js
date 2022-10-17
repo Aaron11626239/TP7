7f43acc0c55f051171e17ad5c52b15251eacaff4
Aaron11626239 <hung-yu.chen2@wsu.edu>
2022-10-17 03:28:19 +0000

    added the recipe object and new recipe for tiramisu

--- scripts.js
+++ scripts.js
@@ -25,8 +25,61 @@ function loadFileInto(fromFile, whereTo) {
 
 }
 
+//new Recipe object
+function Recipe(recipeName, contributorName, imageURL, ingredientsURL, equipmentURL, directionsURL) {
+  
+  this.recipeName = recipeName;
+  this.contributor = contributorName;
+  this.imageURL = imageURL;
+  this.ingredients = ingredientsURL;
+  this.equipment = equipmentURL;
+  this.directions = directionsURL;
+  
+  this.displayRecipe = function() {
+    document.querySelector("#titleBanner h1").innerHTML = this.recipeName;
+    document.querySelector("#contributor").innerHTML = this.contributor;
+    document.querySelector("#titleBanner").style.backgroundImage = "url("+ this.imageURL + ")";
+    loadFileInto(this.ingredients, "#ingredients ul");
+    loadFileInto(this.equipment, "#equipment ul");
+    loadFileInto(this.directions, "#directions ol");
+    
+  }
+}
+
+MomsPolishStewedCabbage = new Recipe("Moms Polish Stewed Cabbage",
+                                     "pretzeld",
+                                     "https://images.pexels.com/photos/8321980/pexels-photo-8321980.jpeg?cs=srgb&dl=pexels-pexels-user-8321980.jpg&fm=jpg", 
+                                     "ingredients.html",
+                                     "equipment.html",
+                                     "directions.html"
+                                    );
+
+BakedAppleCiderDonuts = new Recipe("Baked Apple Cider Donuts",
+                                     "Chef John",
+                                     "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2017/12/15/0/VP0106H_Apple-Cider-Baked-Donuts_s4x3.jpg.rend.hgtvcom.616.462.suffix/1513375696765.jpeg.jpg", 
+                                     "bakedappleciderdonuts - ingredients.html",
+                                     "bakedappleciderdonuts - equipment.html",
+                                     "bakedappleciderdonuts - directions.html"
+                                    );
+
+RutabagaBeefStew = new Recipe("Rutabaga Beef Stew",
+                                     "dinehaus",
+                                     "https://i0.wp.com/tastefoodblog.com/wp-content/uploads/2012/03/irish-stew-tf2.jpg",
+                                     "rutabagabeefstew - ingredients.html",
+                                     "rutabagabeefstew - equipment.html",
+                                     "rutabagabeefstew - directions.html"
+                                    );
 
 window.onload = function() {
   
-
+  document.querySelector("#firstRecipe").onclick = function() {
+    MomsPolishStewedCabbage.displayRecipe();
+  }
+  document.querySelector("#secondRecipe").onclick = function() {
+    BakedAppleCiderDonuts.displayRecipe();
+  }
+  document.querySelector("#thirdRecipe").onclick = function() {
+    RutabagaBeefStew.displayRecipe();
+  }
+  
 }// end window.onload
